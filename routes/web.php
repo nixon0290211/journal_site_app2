ac4efdfe524be2fb0b614ad2dab5ddd248ea97dc
chibatakashi <nixon0290211@icloud.com>
2022-10-19 02:55:11 +0900

    課題１終了

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('articles', App\Http\Controllers\articleController::class);
+// リソースを使用しない場合
+// Route::get('/articles', [App\Http\Controllers\articleController::class, 'index']);
+// Route::get('/articles/create', [App\Http\Controllers\articleController::class, 'create']);
+// Route::post('/articles', [App\Http\Controllers\articleController::class, 'store']);
+// Route::get('/articles/{article}', [App\Http\Controllers\articleController::class, 'show']);
+// Route::get('/articles/{article}/edit', [App\Http\Controllers\articleController::class, 'edit']);
+// Route::patch('/articles/{article}', [App\Http\Controllers\articleController::class, 'update']);
+// Route::delete('/articles/{article}', [App\Http\Controllers\articleController::class, 'destroy']);
+
+
