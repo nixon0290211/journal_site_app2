df6be4b24cd9cef696715a5abc9a1ddfc1817160
chibatakashi <nixon0290211@icloud.com>
2022-10-19 17:12:27 +0900

    第２課題終了

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,25 +1,72 @@
 <?php
 
 namespace App\Http\Controllers;
-// articleクラスを読み込む
+
 use App\Models\Article;
-use Illuminate\Http\Request;
+use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
-{
-    // indexページへ移動
+｛
     public function index()
     {
-        // モデル名::テーブル全件取得
         $articles = Article::all();
-        // articlesティレクトリーの中のindexページを指定し、articlesの連想配列を代入
         return view('articles.index', ['articles' => $articles]);
     }
 
-    // showページへ移動
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store(Request $request)
+    {
+        // インスタンスの作成
+        $article = new Article;
+
+        // 値の用意
+        $article->title = $request->title;
+        $article->body = $request->body;
+
+        // インスタンスに値を設定して保存
+        $article->save();
+
+        // 登録したらindexに戻る
+        return redirect('/articles');
+    }
+
     public function show($id)
     {
         $article = Article::find($id);
-        return view('article.show', ['article' => $article]);
+        return view('articles.show', ['article' => $article]);
     }
-}
+
+    public function edit($id)
+    {
+        $article = Article::find($id);
+        return view('articles.edit', ['article' => $article]);
+    }
+    public function update(Request $request, $id)
+    {
+        // ここはidで探して持ってくる以外はstoreと同じ
+        $article = new Article;
+
+        // 値の用意
+            $article->title = $request->title;
+        $article->body = $request->body;
+
+          // 保存
+        $article->save();
+
+        // 登録したらindexに戻る
+        return redirect('/articles');
+    }
+    public function destroy($id)
+
+
+    // {
+    //     $article = Article::find($id);
+    //     $article->delete();
+
+    //     return redirect('/articles');
+    // }
+    ｝
